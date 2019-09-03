58a5d198b12312f63e9f4cde985870feb59dec34
Dominik Niemiro <nieminik@protonmail.com>
2019-09-03 18:28:01 +0200

    Change MOUSE layout and add LAYER for i3

--- keyboards/planck/keymaps/nieminik/keymap.c
+++ keyboards/planck/keymaps/nieminik/keymap.c
@@ -21,6 +21,7 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _DVORAK,
+  _NUMBERS,
   _MOUSE,
   _LOWER,
   _RAISE,
@@ -42,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,   KC_SLSH,
     KC_BSPC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,   LT(_MOUSE, KC_S),    KC_TAB,
     KC_LSFT, LCTL_T(KC_SCLN), KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    RCTL_T(KC_Z),    RSFT_T(KC_ENT) ,
-    KC_LCTL, KC_LALT, KC_LGUI, KC_RALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT
+    KC_LCTL, LM(_NUMBERS, MOD_LALT), KC_LGUI, KC_RALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT
 ),
 
 
@@ -71,10 +72,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 [_MOUSE] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2,  _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_MS_L, KC_MS_D,  KC_MS_U, KC_MS_R, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_WH_L, KC_WH_D,  KC_WH_U, KC_WH_R, _______,
+    _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U, _______, _______, _______,  _______, _______, _______,
+    _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______, _______, _______,  _______, _______, _______,
+    _______, _______, KC_WH_L, _______, KC_WH_R, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+),
+
+
+[_NUMBERS] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_0
 )
 
 };
